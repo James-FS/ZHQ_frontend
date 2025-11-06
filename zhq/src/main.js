b60f2173e96cc31092cef6eee8e793c041499a2c
yzh <3370157161@qq.com>
2025-11-06 20:28:46 +0800

    feature: 新增按钮公共组件，新增公共防抖函数

--- zhq/src/main.js
+++ zhq/src/main.js
@@ -3,8 +3,12 @@ import {
 } from "vue";
 import App from "./App.vue";
 import './static/styles/global.css';
+import CommonButton from './components/Button.vue';
+
 export function createApp() {
 	const app = createSSRApp(App);
+	// 全局注册公共组件
+	app.component('CommonButton', CommonButton);
 	return {
 		app,
 	};
