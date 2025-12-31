6431c569021516bd1c81bc6f240f8d9133185c56
lcx <a18022690763@qq.com>
2025-12-31 21:04:56 +0800

    weidai

--- zhq/src/utils/index.js
+++ zhq/src/utils/index.js
@@ -6,15 +6,10 @@ export const api = {
 
   // 微信登录
   wechatLogin: (data) => post('/api/v1/auth/login', data),
-<<<<<<< HEAD
-
-=======
   // 手机号+密码登录
   phoneLogin: (data) => post('/api/v1/auth/phone-login', data),
   // 手机号注册
   registerByPhone: (data) => post('/api/v1/auth/register', data),
-  
->>>>>>> develop
   // 用户相关
   getUserProfile: () => get('/api/v1/user/profile'),
   updateUserProfile: (data) => put('/api/v1/user/profile', data),
@@ -26,14 +21,12 @@ export const api = {
   updateTeam: (id, data) => put(`/api/v1/teams/${id}`, data), // 更新队伍信息
   deleteTeam: (id) => del(`/api/v1/teams/${id}`), // 删除队伍
 
-<<<<<<< HEAD
   // 简历相关
   getResumeList: () => get('/api/v1/resumes'), // 获取简历列表
   uploadResume: (data) => post('/api/v1/resumes/upload', data), // 上传简历
   deleteResume: (id) => del(`/api/v1/resumes/${id}`), // 删除简历
-  setDefaultResume: (id) => put(`/api/v1/resumes/${id}/default`) // 设置默认简历
-=======
+  setDefaultResume: (id) => put(`/api/v1/resumes/${id}/default`), // 设置默认简历
+
   //收藏相关
   removeCollection: (id) => del(`/api/v1/user/collection?team_id=${id}`)
->>>>>>> develop
 }
